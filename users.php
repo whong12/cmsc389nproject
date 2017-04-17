41073ec926e3270f3b241925ffbd20f3c40ac88b
dbriggs2 <dbriggs2@umd.edu>
2017-04-17 12:53:25 -0400

    Delete users.php

--- users.php
+++
@@ -1,9 +0,0 @@
-<?
-include("config.php");
-echo "<h2>Users</h2>";
-$sql=$dbh->prepare("SELECT name FROM chatters");
-$sql->execute();
-while($r=$sql->fetch()){
- echo "<div class='user'>{$r['name']}</div>";
-}
-?>
